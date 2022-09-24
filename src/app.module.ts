0386005d365f8b7d02ef71e7d18d3e1d5d25acf9
mikel-js <michaelcastro.js@gmail.com>
2022-09-24 12:00:10 +0300

    feat: add admin and items module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminModule } from './admin/admin.module';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
+  imports: [AdminModule, ItemsModule],
   controllers: [AppController],
   providers: [AppService],
 })
